e3c1041ca0f7c0b09a754c424426d0c8b54f51a6
Jonathan Yates <me@jyates.dev>
2026-04-17 22:01:48 -0700

    Set error caching TTL to 0 to prevent stale API error responses

--- cloudfront/main.tf
+++ cloudfront/main.tf
@@ -262,14 +262,14 @@ resource "aws_cloudfront_distribution" "dist" {
     error_code            = 403
     response_code         = 200
     response_page_path    = "/index.html"
-    error_caching_min_ttl = 10
+    error_caching_min_ttl = 0
   }
 
   custom_error_response {
     error_code            = 404
     response_code         = 200
     response_page_path    = "/index.html"
-    error_caching_min_ttl = 10
+    error_caching_min_ttl = 0
   }
 }
 
