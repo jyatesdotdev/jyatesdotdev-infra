217152abb6ef8704e4a621a15effb6951a081d2e
Jonathan Yates <me@jyates.dev>
2026-04-17 21:38:31 -0700

    Switch static site to SSE-S3 for CloudFront compatibility

--- s3/main.tf
+++ s3/main.tf
@@ -64,8 +64,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "static_site" {
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = var.kms_key_arn
-      sse_algorithm     = "aws:kms"
+      sse_algorithm = "AES256"
     }
   }
 }
