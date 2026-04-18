72d91d41203b6b28496beda0a5aaa91f78544282
Jonathan Yates <me@jyates.dev>
2026-04-17 19:31:04 -0700

    Replace deprecated dynamodb_table with use_lockfile

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "jyatesdotdev-terraform-state"
-    key            = "state/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
+    bucket       = "jyatesdotdev-terraform-state"
+    key          = "state/terraform.tfstate"
+    region       = "us-west-2"
+    use_lockfile = true
+    encrypt      = true
   }
 }
